3c5621cde1354b84901212dae727cea0bc14c916
alizamir25 <ali0786zamir@gmail.com>
2024-10-06 12:38:47 +0100

    Patch

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ function createRing(containerId, percentage){
 	backgroundCircle.setAttribute("stroke", "#0F1121");
 	backgroundCircle.setAttribute("stroke-width", strokeWidth);
 	backgroundCircle.setAttribute("fill", "none");
-	container.innerHTML = "";
+	container.innerHTML="";
 	container.appendChild(backgroundCircle);
-	var foregroundCircle = document.createElementNS(
+	var foregroundCircle=document.createElementNS(
 		"http://www.w3.org/2000/svg",
 		"circle"
 	);
@@ -45,26 +45,26 @@ function createRing(containerId, percentage){
 	foregroundCircle.setAttribute("stroke-dasharray", circumference);
 	foregroundCircle.setAttribute(
 		"stroke-dashoffset",
-		circumference - (circumference * percentage) / 100
+		circumference-(circumference*percentage)/100
 	);
 	foregroundCircle.setAttribute(
 		"transform",
-		"rotate(-90 " + centerX + " " + centerY + ")"
+		"rotate(-90 "+centerX+" "+centerY+")"
 	);
 	container.appendChild(foregroundCircle);
-	var labelText = percentage.toFixed(0) + "%";
-	var label = document.createElementNS("http://www.w3.org/2000/svg", "text");
+	var labelText=percentage.toFixed(0)+"%";
+	var label=document.createElementNS("http://www.w3.org/2000/svg", "text");
 	label.setAttribute("x", centerX);
-	label.setAttribute("y", centerY + 10);
+	label.setAttribute("y", centerY+10);
 	label.setAttribute("fill", "white");
 	label.setAttribute("font-size", "30");
 	label.setAttribute("font-weight", "bolder");
 	label.setAttribute("text-anchor", "middle");
-	label.textContent = labelText;
+	label.textContent=labelText;
 	container.appendChild(label);
 }
 createRing("ring", 0);
-var checkboxes = document.querySelectorAll('.tasks input[type="checkbox"]');
-checkboxes.forEach(function (checkbox) {
+var checkboxes=document.querySelectorAll('.tasks input[type="checkbox"]');
+checkboxes.forEach(function (checkbox){
 	checkbox.addEventListener("change", updateDonut);
 });
