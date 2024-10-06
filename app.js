edba6f578cbb5e6a82d7e9ee2431762dff1b77c8
alizamir25 <ali0786zamir@gmail.com>
2024-10-06 12:37:18 +0100

    Patch

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 	updateDonut();
 });
-
 function updateDonut() {
 	var checkboxes = document.querySelectorAll('.tasks input[type="checkbox"]');
 	var checkedCount = 0;
-
 	checkboxes.forEach(function (checkbox) {
 		if (checkbox.checked) {
 			checkedCount++;
 		}
 	});
-
 	var percentage = (checkedCount / checkboxes.length) * 100;
-
 	createRing("ring", percentage);
 }
-
 function createRing(containerId, percentage) {
 	var container = document.getElementById(containerId);
 	var radius = 135;
 	var strokeWidth = 30;
-
 	var circumference = 2 * Math.PI * radius;
 	var angle = (percentage / 100) * 360;
-
 	var centerX = 150;
 	var centerY = 150;
-
 	var backgroundCircle = document.createElementNS(
 		"http://www.w3.org/2000/svg",
 		"circle"
@@ -40,7 +32,6 @@ function createRing(containerId, percentage) {
 	backgroundCircle.setAttribute("fill", "none");
 	container.innerHTML = "";
 	container.appendChild(backgroundCircle);
-
 	var foregroundCircle = document.createElementNS(
 		"http://www.w3.org/2000/svg",
 		"circle"
@@ -61,7 +52,6 @@ function createRing(containerId, percentage) {
 		"rotate(-90 " + centerX + " " + centerY + ")"
 	);
 	container.appendChild(foregroundCircle);
-
 	var labelText = percentage.toFixed(0) + "%";
 	var label = document.createElementNS("http://www.w3.org/2000/svg", "text");
 	label.setAttribute("x", centerX);
@@ -73,9 +63,7 @@ function createRing(containerId, percentage) {
 	label.textContent = labelText;
 	container.appendChild(label);
 }
-
 createRing("ring", 0);
-
 var checkboxes = document.querySelectorAll('.tasks input[type="checkbox"]');
 checkboxes.forEach(function (checkbox) {
 	checkbox.addEventListener("change", updateDonut);
